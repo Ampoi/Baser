a75587916fa71e77be7fb4c1fd3e98cf5b5004bd
Ampoi <tange.toshihiro@outlook.jp>
2023-09-13 17:28:51 +0900

    カーソルの位置がおかしい問題の修正

--- client/draw/cursor.ts
+++ client/draw/cursor.ts
@@ -1,19 +1,9 @@
 import p5 from "p5"
-import { tileSize } from "../config"
-import { playerPosition } from "../utils/playerPosition"
 import { drawObject } from "./object/index"
 import { images } from "../utils/images"
 
-export const drawCursor =  (p: p5, imageName: keyof typeof images.images, direction: number) => {
+export const drawCursor =  (p: p5, x:number, y:number, imageName: keyof typeof images.images, direction: number) => {
     p.tint(255, 100)
-    const [playerX, playerY] = playerPosition.get()
-
-    const windowStartCornerTileX = Math.floor(playerX - (p.windowWidth / 2) / tileSize)
-    const windowStartCornerTileY = Math.floor(playerY - (p.windowHeight / 2) / tileSize)
-    
-    const mouseTileX = Math.ceil(p.mouseX / tileSize)
-    const mouseTileY = Math.ceil(p.mouseY / tileSize)
-    
-    drawObject(p, mouseTileX + windowStartCornerTileX, mouseTileY + windowStartCornerTileY, 1, imageName, direction)
+    drawObject(p, x, y, 1, imageName, direction)
     p.noTint()
 }
--- client/utils/cursor.ts
+++ client/utils/cursor.ts
@@ -1,6 +1,8 @@
 import p5 from "p5"
 import { Entity } from "../../model/Entity"
 import { drawCursor } from "../draw/cursor"
+import { playerPosition } from "./playerPosition"
+import { tileSize } from "../config"
 
 let selectedIndex = 0
 let tileDirection = 1/2
@@ -22,5 +24,14 @@ export function cursor(p: p5, uid: string, entities: Entity[]){
 
     if( !player.inventory[selectedIndex] ){ selectedIndex = player.inventory.length - 1 }
     const selectedItemName = player.inventory[selectedIndex].name
-    drawCursor(p, selectedItemName, tileDirection)
+
+    const [playerX, playerY] = playerPosition.get()
+
+    const windowStartCornerTileX = Math.floor(playerX - (p.windowWidth / 2) / tileSize)
+    const windowStartCornerTileY = Math.floor(playerY - (p.windowHeight / 2) / tileSize)
+
+    const mouseTileX = Math.ceil(p.mouseX / tileSize - 1/2) + windowStartCornerTileX
+    const mouseTileY = Math.ceil(p.mouseY / tileSize) + windowStartCornerTileY
+
+    drawCursor(p, mouseTileX, mouseTileY, selectedItemName, tileDirection)
 }
